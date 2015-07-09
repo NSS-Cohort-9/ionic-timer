3bc926ebee85a3514ba80b6a6eb8c1c9cdbd1ca3
Zoe Ames <zoeames@gmail.com>
2015-07-09 15:33:50 -0500

    vibration working

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ ionic platform add ios
 ionic build ios
 ionic emulate ios -cl --port=5500 --livereload-port=5501
 ```
+
+##### Plugins to Add
+cordova plugin add org.apache.cordova.vibration
--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,4 +1,23 @@
 angular.module('starter')
-.controller('MainCtrl', ['$scope', function($scope){
-    $scope.message='main controller added';
+.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    $scope.clock=10;
+    var timer;
+
+    function reduceTime(){
+      $scope.clock -= 1;
+
+      if($scope.clock <= 0){
+        navigator.vibrate(3000);
+        $interval.cancel(timer);
+      }
+    }
+
+    $scope.start = function(){
+      timer = $interval(reduceTime, 1000);
+    };
+
+    $scope.reset = function(){
+      $interval.cancel(timer);
+      $scope.clock = 120;
+    }
 }]);
