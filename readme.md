22942e1f0a131c0f3f570356352ad6b063cb752a
Zoe Ames <zoeames@gmail.com>
2015-07-09 11:33:01 -0500

    initial files, main controller added

---
+++ readme.md
@@ -0,0 +1,6 @@
+###### on ios
+```
+ionic platform add ios
+ionic build ios
+ionic emulate ios -cl --port=5500 --livereload-port=5501
+```
---
+++ www/js/mainCtrl.js
@@ -0,0 +1,4 @@
+angular.module('starter')
+.controller('MainCtrl', ['$scope', function($scope){
+    $scope.message='main controller added';
+}]);
